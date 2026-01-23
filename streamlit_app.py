92453b8f6fc5234da992f4e45921ce0692269f2c
Abdulrafiu Omotosho Mohammed <abdulrafiumohammed2019@gmail.com>
2026-01-23 09:37:50 -0800

    Fix formatting of main function definition

--- streamlit_app.py
+++ streamlit_app.py
@@ -548,7 +548,7 @@ def generate_pdf_report(disease_data, image_path=None):
 # -------------------------
 # Main App
 # -------------------------
-def main():
+def main():  
     # Sidebar
     with st.sidebar:
         st.image("https://img.icons8.com/color/96/000000/farm.png", width=80)
